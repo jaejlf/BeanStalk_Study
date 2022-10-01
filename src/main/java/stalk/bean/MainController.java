243b723ce53e4627c7dc3b7a7beea34dab0cdd3a
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-01 17:30:01 +0900

    Update MainController.java

--- src/main/java/stalk/bean/MainController.java
+++ src/main/java/stalk/bean/MainController.java
@@ -2,13 +2,14 @@ package stalk.bean;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
-    @GetMapping("/hi1")
+    @GetMapping("/")
     public String main1() throws Exception{
-        return "/main";
+        return "하이";
     }
 
     @GetMapping("/hi2")
